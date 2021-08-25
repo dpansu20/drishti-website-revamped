9bd09c19b8539683f808f36334d38293ebefe635
Deepanshu Prasad <60698292+dpansu20@users.noreply.github.com>
2021-08-25 12:45:18 +0530

    Navbar Logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,25 @@
 import React from "react"
 
+import Svnit from "../images/svnit.png"
+import DrishtiNav from "../images/drishti_white.png"
+
+import "../css/navbar.css"
+
 function Navbar() {
     return (
-        <div>
-            <h1>This is Navbar</h1>
-        </div>
+        <nav className="navBar">
+            <div className="navBar-brand">
+                <a href ="http://svnit.ac.in/" className="">
+                    <img className="svnit-logo" src={Svnit} alt="SVNIT" />
+                </a>
+                <a href ="http://drishti-svnit.github.io/drishti/" className="drishti-contain">
+                    <img className="drishti-logo" src={DrishtiNav} alt="Drishti" />
+                </a>
+            </div>
+            <div className="navBar-menu">
+                
+            </div>
+        </nav>
     )
 }
 
