3df6828a515b59428721ee93b300e3ea34af0f86
Deepanshu Prasad <60698292+dpansu20@users.noreply.github.com>
2021-09-04 19:06:31 +0530

    Created Routes

--- src/App.js
+++ src/App.js
@@ -1,71 +1,27 @@
 import React from "react"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
-import Drishti from "./Home/drishti"
-import About from "./Home/about.jsx"
-import Gallery from "./Home/gallery.jsx"
-import Event from "./Home/event.jsx"
-import Project from "./Home/project.jsx"
-// import Footer from "./components/Footer"
-
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faChevronDown, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
-
-library.add(fab, faChevronDown, faInfoCircle)
-
-// Set this status value to 1, when notifying about new event, or some recent achievements Drishti.
-// Revert the value to 0, when notice is no more valid, or the event is over.
-var newStatus = 1;
-
-
-// Route links and Options for Homepage
-let homeNav = [
-  {
-    name: "Home",
-    link: "/"
-  }, 
-  {
-    name: "About Us",
-    link: "#about"
-  },
-  {
-    name: "Gallery",
-    link: "#gallery"
-  },
-  {
-    name: "Projects",
-    link: "#projects"
-  },
-  {
-    name: "Team",
-    link: "/team"
-  },
-  {
-    name: "Contact",
-    link: "#footer"
-  }];
-
+import Home from "./pages/Home/"
+import Teams from "./pages/Team/"
 
 
 
 
 function App() {
   
-  if (newStatus === 1){
-    var notice = ["/notice","blink"];
-  }
-  else{
-    notice = ["#","nopop"];
-  }
-
   return (
-    <div className="main">
-      <Drishti nav={ homeNav } notice={notice} />
-      <About />
-      <Gallery />
-      <Event />
-      <Project />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component= {Home} />
+        <Route path = "/home" component = {Home} />
+        <Route path = "/drishti-website-revamped" component = {Home} />
+        <Route path = "/team" component = {Teams} />
+      </Switch>
+    </Router>
   );
 }
 
