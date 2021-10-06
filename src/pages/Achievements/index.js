cae946c308992c31a39e6c31217c86a91b3ce08a
Deepanshu Prasad <60698292+dpansu20@users.noreply.github.com>
2021-10-06 12:10:16 +0530

    Pages initiallized

---
+++ src/pages/Achievements/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+
+function Achievements() {
+
+  return (
+    <div className="main">
+      <h1>Achievements</h1>
+    </div>
+  );
+}
+
+export default Achievements;
---
+++ src/pages/Notices/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+
+function Notices() {
+
+  return (
+    <div className="main">
+      <h1>Notices</h1>
+    </div>
+  );
+}
+
+export default Notices;
