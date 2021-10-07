8c15508110d987e9a2be8cef9ac57e130604c123
Deepanshu Prasad <60698292+dpansu20@users.noreply.github.com>
2021-10-07 06:41:46 +0530

    first division background image to css

--- src/index.js
+++ src/index.js
@@ -2,29 +2,9 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 import "./sass/style.scss"
+import "./css/style.css"
 
 import App from "./App"
-
-
-
-// window.addEventListener("beforeunload", () => {
-//     window.localStorage.setItem(
-//       `lastKnown_${window.location.href}`,
-//       JSON.stringify({
-//         data: document.getElementById("content").innerHTML
-//       })
-//     );
-//   });
-
-
-
-
-
-// if (window.hasRestoredState) {
-//     ReactDOM.hydrate(<App />, document.getElementById('root'));
-// } else {
-//     ReactDOM.render(<App />, document.getElementById('root'));
-// }
 
 
 ReactDOM.render(
